e912ea0e009a98dabd6e25277e50b12f2a17e0f1
Kyyy's Macbook Pro <kyle@Kyyyss-MacBook-Pro.local>
2019-04-05 10:30:46 -0700

    Final

--- location_tests.py
+++ location_tests.py
@@ -11,20 +11,28 @@ class TestLab1(unittest.TestCase):
         loc2 = Location("LA", 0, 180)
         self.assertEqual(repr(loc2),"Location('LA', 0, 180)")
 
-    def test_eq(self):
-        loc3 = Location("San Diego", 106.54, 113.69)
+    def test_eq_are_the_same(self):
         loc1 = Location("San Diego", 106.54, 113.69)
+        loc3 = Location("San Diego", 106.54, 113.69)
+        self.assertTrue(loc1 == loc3)
+
+    def test_eq_have_dif_long(self):
         loc2 = Location("LA", 0, 180) 
         loc4 = Location("LA", 0, -180)
+        self.assertFalse(loc2 == loc4)
+
+    def test_eq_have_dif_lat(self):
         loc5 = Location("LA", 1, 180)
+        loc2 = Location("LA", 0, 180)
+        self.assertFalse(loc2 == loc5)
+
+    def test_eq_have_dif_name(self):
+        loc2 = Location("LA", 0, 180)
         loc6 = Location("la", 0, 180)
         loc7 = Location("SD", 0, 180)
-        self.assertTrue(loc1 == loc3)
-        self.assertFalse(loc1 == loc2)
-        self.assertFalse(loc2 == loc4)
-        self.assertFalse(loc2 == loc5)
         self.assertFalse(loc2 == loc6)
         self.assertFalse(loc2 == loc7)
+
 
 if __name__ == "__main__":
         unittest.main()
